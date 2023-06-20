2c227b0d5cfb1675fc79e3549591b6a0f6c82b90
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-20 14:31:24 -0600

    Fix Linters

--- src/router/MyProfile.js
+++ src/router/MyProfile.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import MyProfileComponent from '../components/myprofile/MyProfile';
 
 const MyProfile = () => (
   <div>
-    <h1>MyProfile</h1>
+    <MyProfileComponent />
   </div>
 );
 
