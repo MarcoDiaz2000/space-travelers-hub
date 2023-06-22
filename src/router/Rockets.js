99a22e49af7b1f9f9a2a2f03ed7aff9675ef0e05
jemba narcisse <cabliveer@gmail.com>
2023-06-22 14:46:37 +0200

    Remove dispatch hook to rocket component

--- src/router/Rockets.js
+++ src/router/Rockets.js
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRockets } from '../redux/rockets/rocketsSlice';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Rocket from '../components/Rocket';
 
 const Rockets = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const { data, isLoading, error } = useSelector((state) => state.rockets);
-  useEffect(() => {
-    dispatch(getRockets());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getRockets());
+  // }, [dispatch]);
   if (error) { return <div>{error.message}</div>; }
   return isLoading ? <div className="loading">Loading ...</div> : (
     <div className="container-list">
