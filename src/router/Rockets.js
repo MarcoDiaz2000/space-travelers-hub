ed51e670ebcc82440ae9764dc4b64b72f99425fd
jemba narcisse <cabliveer@gmail.com>
2023-06-24 02:25:59 +0200

    Remove comments on Rockets component

--- src/router/Rockets.js
+++ src/router/Rockets.js
@@ -3,11 +3,7 @@ import { useSelector } from 'react-redux';
 import Rocket from '../components/Rocket';
 
 const Rockets = () => {
-  // const dispatch = useDispatch();
   const { data, isLoading, error } = useSelector((state) => state.rockets);
-  // useEffect(() => {
-  //   dispatch(getRockets());
-  // }, [dispatch]);
   if (error) { return <div>{error.message}</div>; }
   return isLoading ? <div className="loading">Loading ...</div> : (
     <div className="container-list">
